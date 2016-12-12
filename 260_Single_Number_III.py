3ec8d8a51ebf7a905c20e7c0bb3ffd65ca87a0b1
Sven Wang <bs980201@gmail.com>
2016-12-12 11:46:55 -0800

    260 finished

---
+++ 260_Single_Number_III.py
@@ -0,0 +1,34 @@
+"""260_Single_Number_III.py."""
+
+
+class Solution(object):
+    """class."""
+
+    def singleNumber(self, nums):
+        """:type nums: List[int].
+
+        :rtype: List[int]
+        """
+        result = 0
+        for num in nums:
+            result ^= num
+
+        position = 1
+        # why can't use != 1
+        while result & position == 0:
+            position = position << 1
+
+        ans = [0, 0]
+        for num in nums:
+            if num & position == 0:
+                ans[0] ^= num
+            else:
+                ans[1] ^= num
+
+        return ans
+
+list1 = [5, 6, 10, 10, 3, 3]
+
+instance = Solution()
+result = instance.singleNumber(list1)
+print (result)
