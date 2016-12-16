4b596a9eea8e5eecb61bc49d1e70bb338623daf1
Shao-Fan Wang <svenwang@SVENWANG-MBPR.local>
2016-12-16 11:40:50 -0800

    283 78.19%, pep8 ok

--- 283_Move_Zeroes.py
+++ 283_Move_Zeroes.py
@@ -9,10 +9,22 @@ class Solution(object):
 
         :rtype: void Do not return anything, modify nums in-place instead.
         """
-        for n in nums:
-            print (n)
+        # solution1
+        for i in reversed(range(len(nums))):
+            if nums[i] == 0:
+                nums.append(nums.pop(i))
 
-list1 = [1, 0, 3, 0, 5]
-c1 = Solution()
-c1.moveZeroes(list1)
-print (list1)
+        # solution2
+        # position = 0
+        # for i, num in enumerate(nums):
+        #     if num != 0:
+        #         nums[position], nums[i] = nums[i], nums[position]
+        #         position += 1
+
+
+# list1 = [5,3,2,0,36,0,3]
+# list1 = [0,0,1]
+# print(list1)
+# c1 = Solution()
+# c1.moveZeroes(list1)
+# print (list1)
