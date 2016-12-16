9083c779376529f61d440c2c6b83fd84a64c6c10
Shao-Fan Wang <svenwang@Sven-Wangs-MacBook-Pro.local>
2016-12-15 17:49:29 -0800

    153 54.11%, pep8 ok

--- 153_Find_Minimum_in_Rotated_Sorted_Array.py
+++ 153_Find_Minimum_in_Rotated_Sorted_Array.py
@@ -1,27 +1,27 @@
+"""153_Find_Minimum_in_Rotated_Sorted_Array.py."""
+
+
 class Solution(object):
+    """Class."""
+
     def findMin(self, nums):
         """
-        :type nums: List[int]
+        :type nums: List[int].
+
         :rtype: int
         """
-
-        # mid = int(len(nums)/2)+1
-
-        # nums1 = nums[0:mid]
-        # nums2 = nums[mid:len(nums)]
-
         n = len(nums)
         if n == 1:
             return nums[0]
+
         head = 0
-        tail = n-1
+        tail = n - 1
 
         while head != tail:
-            mid = (head+tail)/2 + 1
-            # mid = head+(tail-head)/2
+            mid = (head + tail) // 2 + 1
             if nums[head] < nums[tail]:
                 return nums[head]
-            if nums[mid] < nums[mid-1]:
+            if nums[mid] < nums[mid - 1]:
                 return nums[mid]
             if nums[head] < nums[mid]:
                 head = mid
@@ -29,7 +29,7 @@ class Solution(object):
                 tail = mid - 1
         return [head]
 
-list1 = [2,3,4,1,5]
-print(list1)
-s = Solution()
-print (s.findMin(list1))
+# list1 = [2,3,4,5,1]
+# print(list1)
+# s = Solution()
+# print (s.findMin(list1))
