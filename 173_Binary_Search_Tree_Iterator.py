045ee8dbbec30d014bba04658deec9e37361bb64
Sven Wang <bs980201@gmail.com>
2016-12-15 16:18:41 -0800

    173 fit pep8

--- 173_Binary_Search_Tree_Iterator.py
+++ 173_Binary_Search_Tree_Iterator.py
@@ -1,43 +1,55 @@
-# Definition for a  binary tree node
+"""173_Binary_Search_Tree_Iterator.py."""
+
+
 class TreeNode:
+    """Definition for a  binary tree node."""
+
     def __init__(self, x):
+        """Init."""
         self.val = x
         self.left = None
         self.right = None
 
+
 class Stack:
-     def __init__(self):
-         self.items = []
+    """Stack."""
 
-     def isEmpty(self):
-         return self.items == []
+    def __init__(self):
+        """Init."""
+        self.items = []
 
-     def push(self, item):
-         self.items.append(item)
+    def isEmpty(self):
+        """IsEmpty."""
+        return self.items == []
 
-     def pop(self):
-         return self.items.pop()
+    def push(self, item):
+        """Push."""
+        self.items.append(item)
+
+    def pop(self):
+        """Pop."""
+        self.items.pop()
 
 
 class BSTIterator:
+    """BSTIterator."""
+
     def __init__(self, root):
+        """Init."""
         self.s = Stack()
         self._putall(root)
 
     def hasNext(self):
+        """Hasnext."""
         return not self.s.isEmpty()
 
     def next(self):
+        """Next."""
         tempnode = self.s.pop()
         self._putall(tempnode.right)
         return tempnode.val
 
-    def _putall(self,node):
-        while node != None:
+    def _putall(self, node):
+        while node is not None:
             self.s.push(node)
             node = node.left
-
-
-# Your BSTIterator will be called like this:
-# i, v = BSTIterator(root), []
-# while i.hasNext(): v.append(i.next())
