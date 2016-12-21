54fae6e59384da612caf0cb6214e440e8a5919b6
Shao-Fan Wang <svenwang@SVENWANG-MBPR.local>
2016-12-21 15:48:49 -0800

    comment on 283

--- 283_Move_Zeroes.py
+++ 283_Move_Zeroes.py
@@ -9,11 +9,14 @@ class Solution(object):
 
         :rtype: void Do not return anything, modify nums in-place instead.
         """
+        # 從list的尾巴開始操作，如果是0就pop並且搬到最後一個位置append
         # solution1
         for i in reversed(range(len(nums))):
             if nums[i] == 0:
                 nums.append(nums.pop(i))
 
+        # 碰到0的時候不操作，非0的時候就把他跟第一個0換位置
+        # 必須儲存第一個0的位置
         # solution2
         # position = 0
         # for i, num in enumerate(nums):
@@ -21,10 +24,8 @@ class Solution(object):
         #         nums[position], nums[i] = nums[i], nums[position]
         #         position += 1
 
-
 # list1 = [5,3,2,0,36,0,3]
 # list1 = [0,0,1]
-# print(list1)
 # c1 = Solution()
 # c1.moveZeroes(list1)
 # print (list1)
